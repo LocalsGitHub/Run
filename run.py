e46bce761dd206936333024ea922d74e210671c8
Local <76016636+LocalsGitHub@users.noreply.github.com>
2021-09-12 09:39:09 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,20 @@
+  GNU nano 2.7.4                                                              File: main.py                                                               Modified  
+
 import subprocess
 
 command = input("Command: ").split()
+
+def compute(cmd: list):
+        base = ""
+        for value in cmd:
+                if "subprocess.call([" not in base:
+                        base += "subprocess.call(["
+                base += f"'{value}',"
+        return base[:-1] + "])"
+
 try:
-    base = "subprocess.call(["
-    for value in command:
-        base += f"'{value}',"
-    finalCmd = base[:-1] + "])"
-    exec(finalCmd)
-    input()
-except Exception as e:
-    print(e)
+        exec(compute(command))
+        input()
+except FileNotFoundError:
+    print("Invalid Command")
     input()

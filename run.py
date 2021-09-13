7828d4bee517dfc1aad6ca75c3ef6db9aafbeee7
Local <76016636+LocalsGitHub@users.noreply.github.com>
2021-09-13 15:45:51 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,19 +1,26 @@
-  GNU nano 2.7.4                                                              File: main.py                                                               Modified  
-
 import subprocess
+
+base = ""
 
 command = input("Command: ").split()
 
-def compute(cmd: list):
-        base = ""
+def compute(cmd: list, execute: bool):
+        if type(execute) is not bool:
+                raise TypeError(f"Expected boolean in second arguement instead of {type(execute)}")
+        elif type(cmd) is not list:
+                raise TypeError(f"Expected list in first arguement instead of {type(cmd)}")
+        global base
         for value in cmd:
                 if "subprocess.call([" not in base:
                         base += "subprocess.call(["
                 base += f"'{value}',"
-        return base[:-1] + "])"
+        if execute:
+                return exec(base[:-1] + "])")
+        else:
+                return base[:-1] + "])"
 
 try:
-        exec(compute(command))
+        compute(cmd=command, execute=True)
         input()
 except FileNotFoundError:
     print("Invalid Command")
